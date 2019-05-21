2389a96d575a162e424cad866c195995811fe1b4
André Karge <andre.karge@gmail.com>
2019-05-21 16:15:41 +0200

    add function to test export of PSIfile to file on disk

--- src/main/java/ButtonAction.java
+++ src/main/java/ButtonAction.java
@@ -2,28 +2,31 @@ import com.intellij.lang.ASTNode;
 import com.intellij.lang.FileASTNode;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
 
 public class ButtonAction extends AnAction {
-    private void iterateOverFilesInProject(Project project) {
+    private void iterateOverFilesInProject(AnActionEvent event) {
         // try to add files at given path to project to be parsed
         // TODO: do we have to add real files to the project? can be enough to just use the PSIfile
         //       However, we will need a way to serialize them to let the python ML do its magic
         //       To sum up: we need serializer and deserializer for the tree
-        createVirtualFilefromPath(project);
+        createVirtualFilefromPath(event);
         // iterate over all files in the given project
-        iterateOverRealFiles(project);
+        iterateOverRealFiles(event.getProject());
     }
 
-    private void createVirtualFilefromPath(Project project) {
+    private void createVirtualFilefromPath(AnActionEvent event) {
         //TODO: change to corpus path
         String filePath = "/home/andre/Documents/parseFolderTest/TestClass.java";
-        PsiFileBuilder psiFileBuilder = new PsiFileBuilder(project);
+        PsiFileBuilder psiFileBuilder = new PsiFileBuilder(event.getProject());
         PsiFile file = psiFileBuilder.getPsiFile(filePath);
+        test(event, file);
     }
 
     private void iterateOverRealFiles(Project project) {
@@ -39,9 +42,14 @@ public class ButtonAction extends AnAction {
         });
     }
 
+    private void test(AnActionEvent e, PsiFile f) {
+        PsiDirectory directory = LangDataKeys.IDE_VIEW.getData(e.getDataContext()).getOrChooseDirectory();
+        // Writes file - but as foo.bar ... TODO
+        directory.add(f);
+    }
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-        Project project = anActionEvent.getProject();
-        iterateOverFilesInProject(project);
+        iterateOverFilesInProject(anActionEvent);
     }
 }
